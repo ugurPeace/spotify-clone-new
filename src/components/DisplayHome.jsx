cdf1069f3242c92c843e967fede6c4abae17a8a8
Ugur <ugurarslanpeace@gmail.com>
2024-07-08 15:33:03 +0300

    initial

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,43 @@
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+
+import Navbar from "./Navbar";
+import SongItem from "./SongItem";
+
+function DisplayHome() {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              image={item.image}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DisplayHome;
